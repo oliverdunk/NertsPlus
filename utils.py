a8eda0b84d8705a0ccd568bdc9c6acc0a934f680
Joe <joseph@intrigued.uk>
2022-05-05 15:37:49 +0100

    Refactor the patch and run Python scripts
    This commit refactors the patch.py script to be nicer to work on 😋
    There are now command line arguments for patch.py:
     --run: "Run the patched Nerts executable"
        (Note: run.py still works as before)
     --nerts_path: "Path to the Nerts executable"
     --skip_download: "Skip downloading de4dot"
     --bin-dir: "Directory to store de4dot and temporary executables"
    
    I haven't tested this on Linux or MacOS yet.
    
    Thanks, Joe

--- utils.py
+++ utils.py
@@ -1,34 +1,63 @@
 import os
+import subprocess
 from pathlib import Path
+import shutil
 import platform
+import sys
 
 system = platform.system()
 
-def get_nerts_path():
-  # Try to find where Nerts is installed
-  if system == "Darwin":
-    nertsPath = Path(os.path.join(os.path.expanduser('~'), "Library/Application Support/Steam/steamapps/common/Nerts Online/NERTS! Online.app/Contents/MacOS/NertsOnline.exe"))
-  elif system == "Windows":
-    nertsPath = Path("C:/Program Files (x86)/Steam/steamapps/common/Nerts Online/NERTS! Online.exe")
 
-  while nertsPath is None or not nertsPath.exists():
+def remove_existing_path(path: Path) -> bool:
+    """
+    Removes a pathlib.Path if it exists.
+
+    returns:
+        - True on success,
+        - False on rmtree error
+    """
+
     try:
-      nertsPath = Path(input("Please enter the path to NertsOnline.exe: "))
-    except KeyboardInterrupt:
-      quit()
-    except:
-      print("")
-      pass
+        if path.exists():
+            shutil.rmtree(path)
+    except Exception:
+        return False
 
-  return nertsPath
+    return True
 
-def run_exe(path, args = None):
-  command = str(path)
 
-  if not args is None:
-    command = command + " " + args
+def get_nerts_path():
+    # Try to find where Nerts is installed
+    if system == "Darwin":
+        nertsPath = Path(os.path.join(os.path.expanduser('~'), "Library/Application Support/Steam/steamapps/common/Nerts Online/NERTS! Online.app/Contents/MacOS/NertsOnline.exe"))
+    elif system == "Windows":
+        nertsPath = Path("C:/Program Files (x86)/Steam/steamapps/common/Nerts Online/NERTS! Online.exe")
 
-  if system == "Darwin":
-    os.system("mono64 " + command)
-  else:
-    os.system(command)
+    while nertsPath is None or not nertsPath.exists():
+        try:
+            nertsPath = Path(input("Please enter the path to NertsOnline.exe: "))
+        except KeyboardInterrupt:
+            sys.exit()
+        except Exception as e:
+            print(e)
+
+    return nertsPath
+
+
+def run_exe(path, args: list[str] = None, cwd=None):
+    command = str(path)
+
+    if args is None:
+        args = []
+
+    if cwd is None:
+        cwd = Path().cwd()
+
+    command_list = [command] + args
+
+    print(f"running {command_list} from {cwd}")
+
+    if system == "Darwin":
+        command_list.insert(0, "mono64")
+
+    subprocess.call(command_list, cwd=cwd, shell=True)
